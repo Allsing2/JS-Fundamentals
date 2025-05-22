41104dc55c8652a6a588fe4beefb2a3c3ff8ac62
Feyisa Kumbi <feyisa025@gmail.com>
2025-05-22 11:38:35 +0300

    Update 1-multi_languages.js

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -1,3 +1,6 @@
-console.log('C is fun');
-console.log('Python is coo1');
-console.log('JavaScript is amazing');
+const lang1 = 'C is fun';
+console.log(lang1);
+const lang2 = 'Python is cool';
+console.log(lang2);
+const lang3 = 'JavaScript is amazing'; 
+console.log(lang3);
